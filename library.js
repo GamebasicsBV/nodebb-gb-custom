62490cdde59e003853908f56b268ca62c859912f
Jurgen <jurgen@gamebasics.nl>
2017-06-06 18:06:10 +0200

    modify all properties on login.

--- library.js
+++ library.js
@@ -5,19 +5,13 @@ var winston = module.parent.require('winston');
 var _ = module.parent.require('underscore');
 var groups = module.parent.require('./groups');
 var controllers = require('./lib/controllers');
+var user = module.parent.require('./user');
+var async = require('async');
 
 var plugin = {
     settings: {
         cookieName: 'forum_token',
         secret: '',
-        payload: {
-            username: "",
-            id: "",
-            picture: "",
-            languagecode: "",
-            crewname: "",
-            crewid: ""
-        },
         isInitialized: false,
         ready: false,
         languageCodePrefix: ""
@@ -31,24 +25,74 @@ var plugin = {
         // Load the settings from the database.
         plugin.reloadSettings(callback);
 
-        winston.info("[gb-custom] plugin initialized");
+        winston.info("[gb-custom] init call finished");
     },
 
-    // Callback that will be called when a user gets created, so we can make him join a category that corresponds to his languagecode.
-    // action:user.create
-    userCreated: function(userObject) {
-        // Decide if the user can join his own language group category on the forum.
-        var languageGroupName = plugin.settings.languageCodePrefix + plugin.settings.payload.languagecode.toUpperCase();
-        winston.info("[gb-custom] User created, checking if we can join an existing language group");
+    // Callback that will be called when a user logs on, so we can update properties likes his profile picture and keep it in sync with osm.
+    userLoggedOn: function(request) {
+        winston.info("[gb-custom] user logged on");
+        console.log(request.req.cookies);
+        var cookies = request.req.cookies;
+
+         // Abort if we could not retrieve the jwt token from the cookie
+        if (!Object.keys(cookies).length || !cookies.hasOwnProperty(plugin.settings.cookieName) || !cookies[plugin.settings.cookieName].length) {
+            winston.info("[gb-custom] No information in the JWT cookie");
+            return;
+        }
+
+        // Retrieve the user info from the jwt cookie using the secret from the plugin settings.
+        var tokenFromCookie = cookies[plugin.settings.cookieName];
+        var token = jwt.verify(tokenFromCookie, plugin.settings.secret);
+
+        // Check if the token can be verified.
+        if(!token) {
+            winston.info("[gb-custom] Token could not be verified");
+            return;
+        }
+        console.log(token);
+        // Strip the "_NL" from the NL userId.
+        var website = token.username.endsWith("_NL")
+                        // Strip the last part of an NL username (_NL)
+                        ? "https://www.onlinesoccermanager.nl/Users/" + token.id.slice(0, -3) + "/Profile"
+                        : "https://www.onlinesoccermanager.com/Users/" + token.id + "/Profile";
+
+        var query = {};
+
+        // Check if we need to update the website.
+        if (website) {
+            winston.info('[session-sharing] Updating website for user with id ' + request.uid + ' to ' + website);
+            query.updateWebsite = async.apply(user.setUserFields, request.uid, {
+                website: website
+            });
+        }
+
+        // Update the avatar of the user on login
+        if (token.picture) {
+            winston.info('[session-sharing] Updating picture for user with id ' + request.uid + ' to ' + token.picture);
+
+            query.updatePicture = async.apply(user.setUserFields, request.uid, {
+                picture: token.picture
+            });
+        }
+
+         // Decide if the user can join his own language group category on the forum.
+        var languageGroupName = plugin.settings.languageCodePrefix + token.languagecode.toUpperCase();
+        winston.info("[gb-custom] Checking if we can join an existing language group");
         // We don't want to create language groups, so check if it exists first.
 
         groups.exists(languageGroupName, function (err, result) {
             if (!err && result) {
-                winston.info("[gb-custom] Joining language group : " + languageGroupName + " for user id: " + userObject.user.uid);
+                winston.info("[gb-custom] Joining language group : " + languageGroupName + " for user id: " + request.uid);
                 // Join the language group for this user.
-                groups.join(languageGroupName, userObject.user.uid, function (err) {});
+                groups.join(languageGroupName, request.uid, function (err) {});
             }
         });
+
+        async.parallel(query, function (err, done) {
+ 			if (err) {
+ 				 winston.error("[gb-custom] Error while updating user info from OSM");
+ 			}	
+ 		});
     },
 
     // Retrieve settings, called inside the init function when the website loads for a user.
@@ -67,76 +111,6 @@ var plugin = {
         });
     },
 
-    // When a user gets created, this filter will be called. In it we will modify and add some of the properties that were located inside our jwt cookie.
-    // filter:user.create
-    setExtraUserInfoFromJwtOnUserCreate: function(postData, callback) {
-        
-        if(!plugin.settings.isInitialized) {
-            winston.info("[gb-custom] info from jwt token not set, can't continue");
-            return;
-        }
-
-        winston.info("[gb-custom] Setting extra user info from json web token");
-
-        // Strip the "_NL" from the NL userId.
-        var website = postData.user.username.endsWith("_NL")
-                        // Strip the last part of an NL username (_NL)
-                        ? "https://www.onlinesoccermanager.nl/Users/" + plugin.settings.payload.id.slice(0, -3) + "/Profile"
-                        : "https://www.onlinesoccermanager.com/Users/" + plugin.settings.payload.id + "/Profile";
-
-        // Set the website property on the forum to his osm profile.
-        postData.user.website = website;
-
-        // Set his forum avatar to his picture on osm.
-        postData.user.picture = plugin.settings.payload.picture;
-    
-        callback(null, postData);
-    },
-
-    /* add a middleware function to every request done to the webserver.
-    * Use it to read the jwt cookie once, and retrieve all info from it for later use. filter:router.page*/
-    addMiddleware: function(req, res, next) {
-        // Dont continue if we already retrieved the info from the jwt cookie.
-        if (plugin.settings.isInitialized) {
-            winston.info("[gb-custom] Plugin not yet initialized.");
-            next();
-            return;
-        }
-        
-        // Abort if we could not retrieve the jwt token from the cookie
-        if (!Object.keys(req.cookies).length || !req.cookies.hasOwnProperty(plugin.settings.cookieName) || !req.cookies[plugin.settings.cookieName].length) {
-            next();
-            winston.info("[gb-custom] No information in the JWT cookie");
-            return;
-        }
-
-        var tokenFromCookie = req.cookies[plugin.settings.cookieName];
-        var token = jwt.verify(tokenFromCookie, plugin.settings.secret);
-
-        // Check if the token can be verified.
-        if(!token) {
-            next();
-            return;
-        }
-        winston.info("[gb-custom] Saving the payload from the jwt token to the plugin settings for later use.");
-
-        // Dont continue if any of the necessary properties are not set.
-        if(!token.username || !token.id || !token.languagecode)
-            return;
-        // Save the info comming from the token into the settings, so we can use it later.
-        plugin.settings.payload =  {
-            username: token.username,
-            id: token.id,
-            picture: token.picture,
-            languagecode: token.languagecode,
-            crewname: token.crewname,
-            crewid: token.crewid
-        };
-        winston.info("[gb-custom] jwt cookie info succesfully initialized");
-        plugin.settings.isInitialized = true;
-        
-        next();
-    },
     // Adds the navigation to plugins dropdown on the admin panel. (filter:admin.header.build)
     addAdminNavigation: function(header, callback) {
         header.plugins.push({
